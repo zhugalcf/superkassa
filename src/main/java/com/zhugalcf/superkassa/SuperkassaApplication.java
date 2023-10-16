edecf2331acda35877af6e07310ff9a5ff7aa1f5
zhugalcf <zhugalcf@gmail.com>
2023-10-16 21:24:37 +0300

    Init project

---
+++ src/main/java/com/zhugalcf/superkassa/SuperkassaApplication.java
@@ -0,0 +1,13 @@
+package com.zhugalcf.superkassa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperkassaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperkassaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhugalcf/superkassa/SuperkassaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhugalcf.superkassa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperkassaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
